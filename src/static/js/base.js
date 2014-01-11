29043394c5715f253c291074019ba38eae106186
Wayne Wang <lastr2d2@gmail.com>
2014-01-11 14:30:23 +0800

    close socket when user leaves. close #29

--- src/static/js/base.js
+++ src/static/js/base.js
@@ -1,4 +1,12 @@
 (function(){
     // use timeago
-    $('.timeago').timeago();
+    $('.timeago').timeago();i
+
+    $(window).bind("beforeunload",function(){
+        // close socket before unload
+        if(page && page.room && page.socket){
+            page.room.socket.close();    
+            page.room.socket = undefined;
+        }
+    }
 }());
--- src/static/js/page.rooms.js
+++ src/static/js/page.rooms.js
@@ -14,6 +14,12 @@ $(document).ready(function(){
         // Thanks to http://stackoverflow.com/a/18134067/553073
         $('body').on('hidden.bs.modal', '.modal', function () {
             $(this).removeData('bs.modal');
+
+            // close socket
+            if (page && page.room && page.room.socket){
+                page.room.socket.close();    
+                page.room.socket = undefined;
+            }
         });
 
         $('.nav button.action.changeUser').on('click',function(){
